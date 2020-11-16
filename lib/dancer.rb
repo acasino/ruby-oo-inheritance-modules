648b3f085172863746b6fa5c270c1da543d9bcb1
Andrew <andrew.casin@gmail.com>
2020-11-15 22:55:04 -0600

    commit tree 26b9655749834e6bf360e57cbbcbe3c5027d27eb

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,18 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+
+class Dancer
+    # include Dance #instance methods in the dancer class
+    # extend MetaDancing #methods intended to be used as class methods in kid and dancer classes
+
+    extend FancyDance::ClassMethods 
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
